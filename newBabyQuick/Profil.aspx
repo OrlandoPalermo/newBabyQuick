d612c2580b983be824440f146be28dfab777faba
OrlandoPalermo <orlando_palermo@live.be>
2014-11-13 17:04:56 +0100

    Amélioration de la stabilité + design.

---
+++ newBabyQuick/Profil.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MainPage.Master" AutoEventWireup="true" CodeBehind="Profil.aspx.cs" Inherits="newBabyQuick.Profil" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="indexP" runat="server">
+    <div class="container">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <asp:Label runat="server" ID="NomPrenom"></asp:Label>
+            </div>
+
+            <div class="panel-body">
+                <p>
+                    <div id="boxParents">
+                        <div class="form-group form-inline">
+                            <label for="">Nombre d'enfants : </label>
+                            <asp:TextBox runat="server" ID="NbEnfant" CssClass="form-control"></asp:TextBox>
+                        </div>
+                    </div>
+
+                    <div id="boxBabysitter">
+                        <div class="form-group form-inline">
+                            <label for="<%= dateDispo.ClientID %>">Vous êtes disponible du : </label>
+                            <asp:TextBox runat="server" ID="dateDispo" CssClass="form-control"></asp:TextBox>
+                            <label for="<%= dateFinDispo.ClientID %>">jusqu'au : </label>
+                            <asp:TextBox runat="server" ID="dateFinDispo" CssClass="form-control"></asp:TextBox>
+                        </div>
+                    </div>
+                </p>
+            </div>
+
+        </div>
+    </div>
+
+</asp:Content>
---
+++ newBabyQuick/Profil.aspx.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <généré automatiquement>
+//     Ce code a été généré par un outil.
+//
+//     Les modifications apportées à ce fichier peuvent provoquer un comportement incorrect et seront perdues si
+//     le code est régénéré.
+// </généré automatiquement>
+//------------------------------------------------------------------------------
+
+namespace newBabyQuick {
+    
+    
+    public partial class Profil {
+        
+        /// <summary>
+        /// Contrôle NomPrenom.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label NomPrenom;
+        
+        /// <summary>
+        /// Contrôle NbEnfant.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox NbEnfant;
+        
+        /// <summary>
+        /// Contrôle dateDispo.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox dateDispo;
+        
+        /// <summary>
+        /// Contrôle dateFinDispo.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox dateFinDispo;
+    }
+}
